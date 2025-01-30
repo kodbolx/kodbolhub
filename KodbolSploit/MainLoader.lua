4fb0a89f886418f6364b4e5f773a34ac04e8b224
kodbolx <brawlstars10ggq@gmail.com>
2025-01-30 16:13:20 +0300

    Update MainLoader.lua

--- KodbolSploit/MainLoader.lua
+++ KodbolSploit/MainLoader.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 6839171747 then     --hotel
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kodbolx/kodbolhub/refs/heads/main/Kodbol%20Hub/Main.lua"))()
+warn("soon")
 elseif game.PlaceId == 6516141723 then      --lobby
-loadstring(game:HttpGet("e"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodbolx/kodbolhub/refs/heads/main/KodbolSploit/lobby.lua"))()
 elseif game.PlaceId == 110258689672367 then   --hotel -
-loadstring(game:HttpGet("hotel-"))	
+warn("soon")
 end
 
 
