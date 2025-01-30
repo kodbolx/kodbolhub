4fd5d2c160647ec45d463b90abaca69a84bc4257
kodbolx <brawlstars10ggq@gmail.com>
2025-01-30 16:04:18 +0300

    Update and rename Main.lua to MainLoader.lua

---
+++ KodbolSploit/MainLoader.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 6839171747 then     --hotel
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodbolx/kodbolhub/refs/heads/main/Kodbol%20Hub/Main.lua"))()
+elseif game.PlaceId == 6516141723 then      --lobby
+loadstring(game:HttpGet("e"))()
+elseif game.PlaceId == 110258689672367 then   --hotel -
+loadstring(game:HttpGet("hotel-"))	
+end
+
+
+--dont skid btw
