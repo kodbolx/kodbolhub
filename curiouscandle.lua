0488460df96d9a38df32cf1d7753d8c40c2f21e1
kodbolx <brawlstars10ggq@gmail.com>
2025-01-31 22:20:27 +0300

    Create curiouscandle.lua

---
+++ curiouscandle.lua
@@ -0,0 +1,4 @@
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
+    local tool = LoadCustomInstance("rbxassetid://71996838490134")
+    tool.Parent = game:GetService("Players").LocalPlayer.Backpack
+    tool.Animations.idle.AnimationId = "rbxassetid://10479585177"
