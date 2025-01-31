7cb2b8b71745362c4cccdf1bfbb37f2c6d86ea70
kodbolx <brawlstars10ggq@gmail.com>
2025-01-31 22:27:19 +0300

    Update guidingcandle.lua

--- guidingcandle.lua
+++ guidingcandle.lua
@@ -2,3 +2,5 @@
     local tool = LoadCustomInstance("rbxassetid://100368646934066")
     tool.Parent = game:GetService("Players").LocalPlayer.Backpack
     tool.Animations.idle.AnimationId = "rbxassetid://10479585177"
+
+    require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Candle maded by kodbol,Discord: [https://discord.gg/76Mq5tQWAV]",true)
