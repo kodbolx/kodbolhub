be6cfad1690ab1e7311d11e5870392db0d877a92
kodbolx <brawlstars10ggq@gmail.com>
2025-01-31 22:28:06 +0300

    Update aroxambushplushie.lua

--- aroxambushplushie.lua
+++ aroxambushplushie.lua
@@ -6,3 +6,5 @@ tool.Activated:Connect(function()
   scream.SoundId = "rbxassetid://18532501108"
   scream:Play()
 end)
+
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Plushie maded by kodbol,Discord: [https://discord.gg/76Mq5tQWAV]",true)
