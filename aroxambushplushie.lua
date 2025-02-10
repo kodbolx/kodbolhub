1a58aa6305ec85fa04b4f51875e46ef998199b4a
kodbolx <brawlstars10ggq@gmail.com>
2025-02-10 15:21:05 +0300

    Update aroxambushplushie.lua

--- aroxambushplushie.lua
+++ aroxambushplushie.lua
@@ -1,5 +1,6 @@
 local tool = game:GetObjects("rbxassetid://87515073977175")[1]
             tool.Parent = game.Players.LocalPlayer.Backpack
+tool.TextureId = "rbxassetid://88369678433359"
 
 tool.Activated:Connect(function()
   local scream = Instance.new("Sound", workspace)
