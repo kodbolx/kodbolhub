0c9b812a7d6df7990e0b6d41923c279f45a8746c
kodbol <brawlstars10ggq@gmail.com>
2025-02-16 13:48:08 +0300

    Update Nightmare Mode.lua

--- Nightmare Mode/Nightmare Mode.lua
+++ Nightmare Mode/Nightmare Mode.lua
@@ -1,1 +1,1 @@
-print("idk what to do")
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Remaking in progress, join discord server for news: [https://discord.gg/76Mq5tQWAV]",true)
