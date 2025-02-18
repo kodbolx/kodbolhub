556bbaca8a6e4e6b13b26d5ddd4ca1f4e89e1f39
kodbol <brawlstars10ggq@gmail.com>
2025-02-18 18:34:26 +0300

    Update Doors functions.lua

--- Utilities/Doors functions.lua
+++ Utilities/Doors functions.lua
@@ -1,6 +1,13 @@
 local functions = {}
 
-
+functions.function MakeSound(id, volume)
+   local sound = Instance.new("Sound", workspace)
+   sound.Volume = volume
+   sound.SoundId = id
+   sound:Play()
+end
 
 
 return functions
+
+MakeSound("rbxassetid://4399694260", 10)
